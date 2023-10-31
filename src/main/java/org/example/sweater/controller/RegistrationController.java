848fbb2e111ebc7f5f1b299094c9330d4132fb78
Sergei Galichev <sergey.galichev@outlook.com>
2023-10-31 21:14:51 +1000

    added registration controller

---
+++ src/main/java/org/example/sweater/controller/RegistrationController.java
@@ -0,0 +1,36 @@
+package org.example.sweater.controller;
+
+import org.example.sweater.domain.Role;
+import org.example.sweater.domain.User;
+import org.example.sweater.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDB = userRepo.findByUsername(user.getUsername());
+        if (userFromDB != null) {
+            model.put("message", "User exists!");
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+        return "redirect:/login";
+    }
+}
