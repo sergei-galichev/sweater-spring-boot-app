d39ca9adaf1ef8c627b3ae9ba658b47f8841d993
Sergei Galichev <sergey.galichev@outlook.com>
2023-11-26 23:25:34 +1000

    change: - added parameter User in "add" method.

--- src/main/java/org/example/sweater/controller/MainController.java
+++ src/main/java/org/example/sweater/controller/MainController.java
@@ -1,7 +1,9 @@
 package org.example.sweater.controller;
 import org.example.sweater.domain.Message;
+import org.example.sweater.domain.User;
 import org.example.sweater.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,10 +31,11 @@ public class MainController {
 
     @PostMapping("/main")
     public String add(
+            @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
             Map<String, Object> model) {
-        Message message =  new Message(text, tag);
+        Message message =  new Message(text, tag, user);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
